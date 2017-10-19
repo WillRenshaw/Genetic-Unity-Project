5eff2935d3a9d6ad57a6287421d0b5f02d8db655
Will Renshaw <william.e.renshaw@gmail.com>
2017-10-19 15:32:31 +0100

    Function & Creature Classes Added
    The base function and creature classes were defined and added.

---
+++ Genetic Project/Assets/Creature.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creature{
+    private string name;
+    public string NAME
+    {
+        get { return name; }
+    }
+
+    private int id;
+    public int ID
+    {
+        get { return id; }
+    }
+
+    private int generation;
+    public int GENERATION
+    {
+        get { return generation; }
+    }
+
+
+    public Function RHF = new Function();
+    public Function LHF = new Function();
+    public Function RKF = new Function();
+    public Function LKF = new Function();
+
+    public float bodyLength = 5f;
+    public float RUpperLegLength = 2f;
+    public float LUpperLegLength = 2f;
+    public float RLowerLegLength = 1f;
+    public float LLowerLegLength = 1f;
+
+    public float fitness = 0f;
+
+    public Creature(string n, int gen, int IDnum)
+    {
+        name = n;
+        generation = gen;
+        id = IDnum;
+    }
+
+}
---
+++ Genetic Project/Assets/Function.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Function {
+    enum WaveType
+    {
+        Sin,
+        Triangle
+    }
+
+    WaveType waveType = new WaveType();
+    float amplitude;
+    float wavelength;
+    float phase;
+
+    public Function(int type, float amp, float length, float pha)
+    {
+        waveType = (WaveType)type;
+        
+    }
+
+    public float GetValue(float t)
+    {
+        if(waveType == WaveType.Sin)
+        {
+            return (amplitude * Mathf.Sin((2 * Mathf.PI * t - phase)/amplitude));
+        }else
+        {
+            return ((2 * amplitude / Mathf.PI) * Mathf.Asin(Mathf.Sin((2 * Mathf.PI * t - phase) / amplitude)));
+        }
+    }
+}
