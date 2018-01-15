097d95bdc8bf0be0641924af810888d07422e963
Will Renshaw <william.e.renshaw@gmail.com>
2018-01-15 21:42:38 +0000

    Added numbers

--- Genetic Project/Assets/GraphManager.cs
+++ Genetic Project/Assets/GraphManager.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GraphManager : MonoBehaviour {
 
     float maxHeight = 50; //Height up / down from 0
     float maxWidth = 160; //Width from 0 onwards
 	public Transform linePrefab;
+	public Transform lineMarker;
     private LineRenderer meanLine;
 	private LineRenderer maxLine;
 	private LineRenderer minLine;
@@ -16,10 +18,16 @@ public class GraphManager : MonoBehaviour {
 	{
 		meanLine = Instantiate (linePrefab, transform).GetComponent<LineRenderer> ();
 		meanLine.gameObject.name = "Mean";
+		meanLine.startColor = Color.red;
+		meanLine.endColor = Color.red;
 		maxLine = Instantiate (linePrefab, transform).GetComponent<LineRenderer> ();
 		maxLine.gameObject.name = "Max";
+		maxLine.startColor = Color.blue;
+		maxLine.endColor = Color.blue;
 		minLine = Instantiate (linePrefab, transform).GetComponent<LineRenderer> ();
 		minLine.gameObject.name = "Min";
+		minLine.startColor = Color.green;
+		minLine.endColor = Color.green;
 		DrawLines (Helper.savedGenerations.ToArray ());
 	}
 
@@ -42,6 +50,11 @@ public class GraphManager : MonoBehaviour {
 		maxLine.positionCount = coords.Count;
 		minLine.positionCount = coords.Count;
 		foreach (Coordinate c in coords) {
+			if (c.genNum % 5 == 0 || c.genNum == 1) {
+				Transform tag = Instantiate (lineMarker,transform);
+				tag.transform.position = new Vector3 ((c.genNum - 1) * xToWorldSpace, 0, 0);
+				tag.GetComponentInChildren<Text> ().text = c.genNum.ToString();
+			}
 			meanLine.SetPosition (c.genNum - 1, new Vector3 ((c.genNum - 1) * xToWorldSpace, c.mean * yToWorldSpace, 0));
 			maxLine.SetPosition (c.genNum - 1, new Vector3 ((c.genNum - 1) * xToWorldSpace, c.max * yToWorldSpace, 0));
 			minLine.SetPosition (c.genNum - 1, new Vector3 ((c.genNum - 1) * xToWorldSpace, c.min * yToWorldSpace, 0));
