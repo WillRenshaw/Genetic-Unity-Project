461e38df50a21f93218e3b2ca5caa5a07e9a3ead
Will Renshaw <william.e.renshaw@gmail.com>
2018-01-20 19:08:44 +0000

    Help Menu option added and option sliders changed

--- Genetic Project/Assets/Scripts/GraphManager.cs
+++ Genetic Project/Assets/Scripts/GraphManager.cs
@@ -5,8 +5,6 @@ using UnityEngine.UI;
 
 public class GraphManager : MonoBehaviour {
 
-    float maxHeight = 50; //Height up / down from 0
-    float maxWidth = 160; //Width from 0 onwards
 	public Transform linePrefab;
 	public Transform lineMarker;
     private LineRenderer meanLine;
@@ -37,28 +35,26 @@ public class GraphManager : MonoBehaviour {
 		foreach (Generation gen in gens) { //Convert all gens to coordinates
 			coords.Add(new Coordinate(){genNum = gen.GENNUMBER, mean = gen.MEANFITNESS, max = gen.MAXFITNESS, min = gen.MINFITNESS});
 		}
-
-		float xToWorldSpace = maxWidth / coords.Count; //Constant to convert coordinate x vals to unity world space
-		float maxY = 0;
-		foreach (Coordinate c in coords) { //Find the greatest max value
-			if (c.max > maxY) {
-				maxY = c.max;
-			}
-		}
-		float yToWorldSpace = maxHeight / maxY; //Constant to convert coordinate y vals to unity world space
 		meanLine.positionCount = coords.Count; //Set the size of the position arrays appropriately
 		maxLine.positionCount = coords.Count;
 		minLine.positionCount = coords.Count;
 		foreach (Coordinate c in coords) {
-			if (c.genNum % 5 == 0 || c.genNum == 1) { //Place marker tags for every 5th generation
+			if (c.genNum % 10 == 0 || c.genNum == 1) { //Place marker tags for every 5th generation
 				Transform tag = Instantiate (lineMarker,transform);
-				tag.transform.position = new Vector3 ((c.genNum - 1) * xToWorldSpace, 0, 0);
+				tag.transform.position = new Vector3 ((c.genNum - 1), -2.5f, 0);
 				tag.GetComponentInChildren<Text> ().text = c.genNum.ToString();
 			}
-			meanLine.SetPosition (c.genNum - 1, new Vector3 ((c.genNum - 1) * xToWorldSpace, c.mean * yToWorldSpace, 0)); //Set mean Position
-			maxLine.SetPosition (c.genNum - 1, new Vector3 ((c.genNum - 1) * xToWorldSpace, c.max * yToWorldSpace, 0)); //Set max Position
-			minLine.SetPosition (c.genNum - 1, new Vector3 ((c.genNum - 1) * xToWorldSpace, c.min * yToWorldSpace, 0)); //Set min Position
-
+			meanLine.SetPosition (c.genNum - 1, new Vector3 ((c.genNum - 1), c.mean, 0)); //Set mean Position
+			maxLine.SetPosition (c.genNum - 1, new Vector3 ((c.genNum - 1), c.max, 0)); //Set max Position
+			minLine.SetPosition (c.genNum - 1, new Vector3 ((c.genNum - 1), c.min, 0)); //Set min Position
+		}
+		for (int i = 0; i <= 100; i++) { //Set tags on y axis every 5 units
+			Transform tag = Instantiate (lineMarker,transform);
+			tag.transform.position = new Vector3 (-5, 5 * i, 0);
+			tag.GetComponentInChildren<Text> ().text = (5 * i).ToString();
+			tag = Instantiate (lineMarker,transform);
+			tag.transform.position = new Vector3 (-5, -5 * i, 0);
+			tag.GetComponentInChildren<Text> ().text = (-5 * i).ToString ();
 		}
 	}
 
